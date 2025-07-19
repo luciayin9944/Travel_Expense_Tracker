7b985b01b35b09b82a8d552b99044076b95f8e96
luciayin9944 <luxiayin94@gmail.com>
2025-07-18 19:29:11 -0700

    add signup/login

--- server/app.py
+++ server/app.py
@@ -4,6 +4,48 @@ from flask import Flask
 from flask_restful import Api
 from config import app, db, api, jwt
 from models import User, Trip, Expense, UserSchema, TripSchema, ExpenseSchema
+from flask import request, session, jsonify, make_response
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import create_access_token, get_jwt_identity, verify_jwt_in_request, jwt_required
+
+
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User(username=username)
+        user.password_hash = password
+
+        try:
+            db.seesion.add(user)
+            db.seesion.commit()
+            access_token = create_access_token(identity=str(user.id))
+            response = make_response(jsonify(token=access_token, user=UserSchema().dump(user)), 200)
+            return response
+        except IntegrityError:
+            return {'errors': ['422 Unprocessable Entity']}, 422
+        
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json
+        username = data.get('username')
+        password = data.get('password')
+
+        user = User.query.filter(User.username==username).first()
+
+        if user and user.authenticate(password):
+            access_token = create_access_token(identity=str(user.id))
+            response = make_response(jsonify(token=access_token, user=UserSchema().dump(user)), 200)
+
+
+
+
+
+
 
 
 # Register resources
